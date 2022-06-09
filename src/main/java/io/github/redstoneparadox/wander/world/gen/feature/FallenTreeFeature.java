2c712d015a763eab526a0397e0b7814aff8557d1
RedstoneParadox <spinjitzu00@gmail.com>
2022-06-09 13:05:09 -0700

    Made fallen log placement less floaty

--- src/main/java/io/github/redstoneparadox/wander/world/gen/feature/FallenTreeFeature.java
+++ src/main/java/io/github/redstoneparadox/wander/world/gen/feature/FallenTreeFeature.java
@@ -7,6 +7,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.PillarBlock;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.world.Heightmap;
 import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.util.FeatureContext;
@@ -46,10 +47,13 @@ public class FallenTreeFeature extends Feature<FallenTreeFeatureConfig> {
 			}
 		}
 
+		if (!worldAccess.getBlockState(origin.down()).isSolidBlock(worldAccess, origin.down())) return false;
+
 		worldAccess.setBlockState(origin, stump, Block.NOTIFY_ALL);
 
 		for (int i = 0; i < height; i++) {
 			BlockPos pos = origin.offset(direction, i + 2);
+			pos = worldAccess.getTopPosition(Heightmap.Type.OCEAN_FLOOR_WG, pos);
 			BlockState log = config.log().getBlockState(random, pos);
 
 			if (log.getBlock() instanceof PillarBlock) {
